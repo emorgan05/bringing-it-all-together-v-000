1ef27d0bb5b964e4a2bb852a65144d983c7665ac
Emily Morgan <emorgan05@gmail.com>
2017-10-24 19:48:22 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -42,10 +42,18 @@ class Dog
 
   def self.find_or_create_by(name:, breed:)
     sql = "SELECT * FROM dogs WHERE name = ? AND breed = ?"
-    
-    dog = DB[:conn].execute(sql, )
+
+    dog = DB[:conn].execute(sql, name, breed)
+
+    if !dog.empty?
+      dog_data = dog[0]
+      dog = self.new_from_db(dog_data)
+    else
+      dog = self.create(name: name, breed: breed)
+    end
+    dog
   end
-  
+
   def self.new_from_db(array)
     attributes_hash = { id: array[0], name: array[1], breed: array[2] }
     dog = self.new(attributes_hash)
